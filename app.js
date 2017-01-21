9b7fdb8d9f414851713d4697de9949a45bf62a46
Denis Maslennikov <nof1000@mail.ru>
2017-01-21 10:55:48 +0300

    fix lack of text

--- app.js
+++ app.js
@@ -12,22 +12,24 @@ const config = require('./config.js');
 
 /** Implementation */
 telekit(config).on('message', (context) => {
-    if (context.update.text.indexOf('/ping') > -1) {
-        context.chat.sendMessage({
-            text: '/pong',
-        });
-    }
+    if (context.update.text) {
+        if (context.update.text.indexOf('/ping') > -1) {
+            context.chat.sendMessage({
+                text: '/pong',
+            });
+        }
 
-    if (context.update.text.indexOf('/pong') > -1) {
-        context.chat.sendMessage({
-            text: '/ping',
-        });
-    }
+        if (context.update.text.indexOf('/pong') > -1) {
+            context.chat.sendMessage({
+                text: '/ping',
+            });
+        }
 
-    if (context.update.text.indexOf('/help') > -1) {
-        context.chat.sendMessage({
-            text: 'Source code on [github](https://github.com/telekits/telekit-example-ping) 😊',
-            parse_mode: 'markdown',
-        });
+        if (context.update.text.indexOf('/help') > -1) {
+            context.chat.sendMessage({
+                text: 'Source code on [github](https://github.com/telekits/telekit-example-ping) 😊',
+                parse_mode: 'markdown',
+            });
+        }
     }
 });
